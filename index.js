6820a0a3922088b5aa444351c62860d59d234a26
milagro-g <milagrolyew@gmail.com>
2019-06-27 15:50:30 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+ console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+ response.sendFile(__dirname + '/public/index.html')
+});
